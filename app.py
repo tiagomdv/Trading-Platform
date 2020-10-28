6d0f2e21e3f487357857fafa9a89a72cf1085b87
Tiago Vieira <tiagomdv@hotmail.com>
2020-10-28 20:23:07 +0000

    Login/Logout/Register functionality working

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, redirect
 from tempfile import mkdtemp
 from flask_session import Session
 import sqlite3
@@ -20,21 +20,26 @@ Session(app)
 
 @app.route('/')
 def hello_world():
-    record = "NOTHING"
+    quote = lookup("TSLA")
+    usernames = []
 
+    # _______________________________
     # Acess to database
     dbConnection = sqlite3.connect('finance.db') # connection
     cursor = dbConnection.cursor()
-            
-    cursor.execute("SELECT username FROM users;")
-    record = cursor.fetchall()
 
+    cursor.execute("SELECT username FROM users")
+    user = cursor.fetchall() # gets the output from query
+
+    # Closes connections to database
     cursor.close()
     dbConnection.close()
+    # _______________________________
 
-    quote = lookup("TSLA")
+    for row in user:
+        usernames.append(row[0])
 
-    return render_template("index.html", quote=quote, record=record)
+    return render_template("index.html", quote="TESLA", usernames=usernames)
 
 @app.route('/market')
 def market_func():
@@ -46,44 +51,108 @@ def wallet():
     quote = lookup("AMZN")
     return render_template("wallet.html", quote=quote)
 
-@app.route("/login", methods=["GET", "POST"]) # LOGIN
-def login():
+@app.route("/login", methods=["GET", "POST"]) # LOGIN REGISTER
+def login(): 
 
-    if request.method == "POST":
+    if request.method == "GET":
+        return render_template("login.html")
 
+    if request.method == "POST": # the method POST can be used to LOGIN or to REGISTER. How the code detects is if password2 variable exists
+        
+        # Forget any user id
+        session.clear()
+
+        # ______________________________
         # Checks for valid username and password
         username = request.form.get("username")
-        if username == "":
-            msg = "username."
-            return render_template("login.html", username=username, msg=msg)
-        
         password = request.form.get("password")
-        if password == "":
-            msg = "password."
-            return render_template("login.html", username=username, password=password, msg=msg)
+        if not username:
+            return render_template("login.html", logError="Invalid username!")
 
+        if not password:
+            return render_template("login.html", username=username,logError="Invalid password!")         
+        # _______________________________                
+
+        # Forget any user id
+        session.clear()
+
+        # _______________________________
         # Acess to database
         dbConnection = sqlite3.connect('finance.db') # connection
         cursor = dbConnection.cursor()
 
-
-        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
-        dbConnection.commit() # apply changes
+        cursor.execute("SELECT username, password FROM users WHERE username=?",(username,))
+        user = cursor.fetchall() # gets the output from query
 
         # Closes connections to database
         cursor.close()
         dbConnection.close()
+        # _______________________________
 
-        
-        return render_template("login.html", username=username, password=password)
+        # Checks if already exist username in database
+        if len(user) > 0:
+            session["user_id"] = user[0][0]
+            checkPass = user[0][1]
+        else:
+            return render_template("login.html", logError="User does NOT exist!") 
 
-    msg = "information."
-    type = request.args.get("type")
-    
-    if type == "register":  
-        return render_template("login.html", isreg=1)
+        if not checkPass == password:
+            session.clear()
+            return render_template("login.html", logError="Password is INCORRECT!")              
+ 
 
-    #if type == "registerAcc":
-        #TODO
-    
-    return render_template("login.html", msg=msg)
+        return render_template("index.html", username=username, password=password)   
+
+
+@app.route("/register", methods=["GET", "POST"]) # Handles REGISTRY
+def register():
+
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":   
+        # ______________________________
+        # Checks for valid username and password
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if not username:
+            return render_template("register.html", logError="Invalid username!")
+
+        if not password:
+            return render_template("register.html", username=username, logError="Invalid password!")         
+        # _______________________________
+
+        checkPass = request.form.get("checkPass")
+        if checkPass == password:                  
+            # _______________________________
+            # Acess to database
+            dbConnection = sqlite3.connect('finance.db') # connection
+            cursor = dbConnection.cursor()
+
+            cursor.execute("SELECT username FROM users WHERE username=?",(username,))
+            user = cursor.fetchall() # gets the output from query
+            if user:
+                return render_template("register.html", logError=username + " already exists!")
+                # Closes connections to database
+                cursor.close()
+                dbConnection.close()
+
+            # Inserts data into database
+            cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+            dbConnection.commit() # apply changes
+
+            # Closes connections to database
+            cursor.close()
+            dbConnection.close()
+            # _______________________________
+
+            return render_template("index.html", username=username, password=password, isreg=1)
+        else:
+            return render_template("register.html", username=username, logError="Passwords do NOT match!")
+
+@app.route("/logout", methods=["GET"]) # Handles LOGOUT
+def logout():
+
+    session.clear()
+    return redirect("/") 
